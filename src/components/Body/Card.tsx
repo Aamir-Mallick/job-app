f208f59010987fbfd1b5c55936b57b68a0d22f7a
Souparna_7 <souparna7781@gmail.com>
2024-12-20 20:46:04 +0530

    Initial Level changes

---
+++ src/components/Body/Card.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card-tile'>
+        <div className='card-tile-top'>
+            <div className='category'>IT</div>
+            <div className='role'><span>Tech Lead</span></div>
+        </div>
+        <div className='card-tile-middle'>
+            <div><span>Skills: Vehicle Transmission System</span></div>
+            <div><span>Experience: 2 - 8 Years</span></div>
+            <div><span>Location: PUNE</span></div>
+        </div>
+        <div className='card-tile-bottom'>
+            <Button variant="outlined">APPLY/SHORTLIST</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,17 +5,28 @@ import Button from "@mui/material/Button";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <div>
-        {" "}
-        <img src={logo} alt="logo" />{" "}
+    <div className="header">
+      <div className="header-container">
+        <div>
+          {" "}
+          <img src={logo} alt="logo" />{" "}
+        </div>
+        <div>
+          <div className="header-menu">
+            <p className="item">Life in tech</p>
+            <p className="item">get Started</p>
+            <p className="item">alumni</p>
+            <p className="item">contact us</p>
+            <Button variant="outlined">sign up/login</Button>
+          </div>
+        </div>
       </div>
-      <div className="header-menu">
-        <p className="item">Life in tech</p>
-        <p className="item">get Started</p>
-        <p className="item">alumni</p>
-        <p className="item">contact us</p>
-        <Button variant="outlined">sign up/login</Button>
+      <div className="warning-bar">
+        <div>
+          <b>Scam Alert :</b>
+          To all Tech Mahindra job aspirants and applications, beware of fraudulent job offers
+          <a href="/images/IT_Fraud/Recruitment_Fraud.pdf" target="_blank">Learn More.. </a>
+        </div>
       </div>
     </div>
   );
