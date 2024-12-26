e83b2bca2236d6ec56feee5dbc2e6e6fefd438df
Souparna_7 <souparna7781@gmail.com>
2024-12-26 20:05:07 +0530

    Registration From changes

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "@mui/material/Button";
+import BodyForm from "./BodyForm";
 import "./body.css";
 import Card from "./Card";
 
@@ -9,47 +10,52 @@ const Body = () => {
     setActive(!active);
   };
   return (
-    <div className="main-body">
-      <div className="main-body-top">
-        <div>
-          <p className="recent-jobs">Recent Jobs</p>
-        </div>
-        <div className="filter">
-          <div
-            className={`${active ? "active" : "not-active"}`}
-            onClick={handleClick}
-          >
-            IT
-          </div>
-          <div
-            className={`${!active ? "active" : "not-active"}`}
-            onClick={handleClick}
-          >
-            BPS
-          </div>
-        </div>
-        <div>
+    <div>
+      <div className="main-body">
+        <div className="main-body-top">
           <div>
-            <p className="advance-search">Advance Search</p>
+            <p className="recent-jobs">Recent Jobs</p>
           </div>
-          <div className="search">
-            <div>
-              <input placeholder="Enter keyword like (Job Title or Skills or Experience)" />
+          <div className="filter">
+            <div
+              className={`${active ? "active" : "not-active"}`}
+              onClick={handleClick}
+            >
+              IT
             </div>
-            <div>
-              <Button variant="outlined">SEARCH</Button>
+            <div
+              className={`${!active ? "active" : "not-active"}`}
+              onClick={handleClick}
+            >
+              BPS
             </div>
+          </div>
+          <div>
+            <div>
+              <p className="advance-search">Advance Search</p>
+            </div>
+            <div className="search">
+              <div>
+                <input placeholder="Enter keyword like (Job Title or Skills or Experience)" />
+              </div>
+              <div>
+                <Button variant="outlined">SEARCH</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="main-body-content">
+          <div className="card-container">
+            {Array(17)
+              .fill("")
+              .map(() => {
+                return <Card />;
+              })}
           </div>
         </div>
       </div>
-      <div className="main-body-content">
-        <div className="card-container">
-          {Array(17)
-            .fill("")
-            .map(() => {
-              return <Card />;
-            })}
-        </div>
+      <div className="main-body-form">
+        <BodyForm />
       </div>
     </div>
   );
